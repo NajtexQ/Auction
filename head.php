77fc58a70baf84f33ce814bcd13c03af434c7c0e
Najtex <timi.kompi@gmail.com>
2022-12-13 11:31:54 +0100

    Mobile responsive

--- head.php
+++ head.php
@@ -1,4 +1,5 @@
 <head>
     <title><?php echo $PAGE_TITLE ?></title>
     <link rel="stylesheet" href="<?php echo rootUrl("/css/style.css") ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
